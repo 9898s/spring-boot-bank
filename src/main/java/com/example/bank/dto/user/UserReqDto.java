3c7fc702ce471114dce1453276ff2dc4b25563ce
9898s <sueh9898@icloud.com>
2023-10-16 17:51:38 +0900

    feat: 회원가입 컨트롤러 정규표현식 DTO 적용

--- src/main/java/com/example/bank/dto/user/UserReqDto.java
+++ src/main/java/com/example/bank/dto/user/UserReqDto.java
@@ -3,6 +3,8 @@ package com.example.bank.dto.user;
 import com.example.bank.domain.user.User;
 import com.example.bank.domain.user.UserEnum;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,16 +15,20 @@ public class UserReqDto {
   @Getter
   public static class JoinReqDto {
 
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
     @NotEmpty // null이거나, 공백일 수 없다.
     private String username;
 
     @NotEmpty
+    @Size(min = 4, max = 20)
     private String password;
 
     @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요")
     private String email;
 
     @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
     private String fullname;
 
     public User toEntity(BCryptPasswordEncoder passwordEncoder) {
--- src/test/java/com/example/bank/temp/RegexTest.java
+++ src/test/java/com/example/bank/temp/RegexTest.java
@@ -10,41 +10,63 @@ public class RegexTest {
   void 한글만된다_test() throws Exception {
     String value = "한글";
     boolean result = Pattern.matches("^[가-힣]+$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
   }
 
   @Test
   void 한글은안된다_test() throws Exception {
     String value = "abc";
     boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
   }
 
   @Test
   void 영어만된다_test() throws Exception {
     String value = "abc";
     boolean result = Pattern.matches("^[a-zA-Z]+$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
   }
 
   @Test
   void 영어만안된다_test() throws Exception {
     String value = "가22";
     boolean result = Pattern.matches("^[^a-zA-Z]*$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
   }
 
   @Test
   void 영어와숫자만된다_test() throws Exception {
     String value = "asd123!";
     boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
   }
 
   @Test
   void 영어만되고_길이는최소2최대4이다_test() throws Exception {
     String value = "abc";
     boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
-    System.out.println("테스트: "+ result);
+    System.out.println("테스트: " + result);
+  }
+
+  @Test
+  void user_username_test() throws Exception {
+    String username = "ssar";
+    boolean result = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username);
+    System.out.println("테스트: " + result);
+  }
+
+  @Test
+  void user_fullname_test() throws Exception {
+    String username = "쌀";
+    boolean result = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", username);
+    System.out.println("테스트: " + result);
+  }
+
+  @Test
+  void user_email_test() throws Exception {
+    String username = "ssar@nate.com";
+    boolean result =
+        Pattern.matches("^[a-zA-Z0-9]{2,6}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", username);
+    System.out.println("테스트: " + result);
   }
 }
