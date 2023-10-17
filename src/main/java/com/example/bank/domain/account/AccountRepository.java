342f3cd15f697338962a064745e8ab0b1629a8b8
9898s <sueh9898@icloud.com>
2023-10-17 11:39:18 +0900

    feat: 본인계좌목록보기 서비스 만들기

--- src/main/java/com/example/bank/domain/account/AccountRepository.java
+++ src/main/java/com/example/bank/domain/account/AccountRepository.java
@@ -1,9 +1,12 @@
 package com.example.bank.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
   Optional<Account> findByNumber(Long number);
+
+  List<Account> findByUser_Id(Long id);
 }
--- src/main/java/com/example/bank/service/AccountService.java
+++ src/main/java/com/example/bank/service/AccountService.java
@@ -7,8 +7,13 @@ import com.example.bank.domain.user.UserRepository;
 import com.example.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import com.example.bank.dto.account.AccountResDto.AccountSaveResDto;
 import com.example.bank.handler.ex.CustomApiException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +24,45 @@ public class AccountService {
 
   private final AccountRepository accountRepository;
   private final UserRepository userRepository;
+
+  public AccountListResDto 계좌목록보기_유저별(Long userId) {
+    User userPS = userRepository.findById(userId)
+        .orElseThrow(() -> new CustomApiException("유저를 찾을 수 없습니다."));
+
+    // 유저의 모든 계좌 목록
+    List<Account> accountListPS = accountRepository.findByUser_Id(userId);
+    return new AccountListResDto(userPS, accountListPS);
+  }
+
+  @Setter
+  @Getter
+  public static class AccountListResDto {
+
+    private final String fullname;
+    private List<AccountDto> accounts = new ArrayList<>();
+
+    public AccountListResDto(User user, List<Account> accounts) {
+      this.fullname = user.getFullname();
+      this.accounts = accounts.stream()
+          .map(AccountDto::new)
+          .collect(Collectors.toList());
+    }
+
+    @Setter
+    @Getter
+    public class AccountDto {
+
+      private final Long id;
+      private final Long number;
+      private final Long balance;
+
+      public AccountDto(Account account) {
+        this.id = account.getId();
+        this.number = account.getNumber();
+        this.balance = account.getBalance();
+      }
+    }
+  }
 
   @Transactional
   public AccountSaveResDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
